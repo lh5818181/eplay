31914643a44dfb2672130075115f2cf25a716c4d
Luis Henrique <luish7572@gmail.com>
2025-05-20 23:07:27 -0300

    criando header

---
+++ src/styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
+
+export const Cores = {
+  branca: '#EEEEEE',
+  preta: '#111',
+  cinza: '#333',
+  verde: '#10AC84'
+}
+
+export const GlobalCss = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+    list-style: none;
+    }
+
+  body {
+    background-color: ${Cores.preta};
+    color: ${Cores.branca};
+    padding-top: 40px;
+
+  `
+
+export const Container = styled.div`
+  max-width: 1024px;
+  margin: 0 auto;
+  width: 100%;
+`
