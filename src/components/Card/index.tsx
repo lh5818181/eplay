fbabe3318049d296bdab87821fcf06b63da2f3f0
Luis Henrique <luish7572@gmail.com>
2025-07-02 08:28:15 -0300

    criando checkout e card

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,7 @@
+const Card = () => (
+  <div>
+    <h2>Nome do card</h2>
+  </div>
+)
+
+export default Card
---
+++ src/pages/Checkout/index.tsx
@@ -0,0 +1,10 @@
+import Card from '../../components/Card'
+
+const Checkout = () => (
+  <div className="container">
+    <Card />
+    <Card />
+  </div>
+)
+
+export default Checkout
