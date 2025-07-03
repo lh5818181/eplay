3ea9bb96a199033b05fc7dde543977dd602a4d79
Luis <lh5818181@gmail.com>
2025-07-03 14:44:26 -0300

    criando card de pagamento

--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,13 +1,25 @@
 import styled from 'styled-components'
 import { Cores } from '../../styles'
 
-export const Row = styled.div`
+type InputGroupProps = {
+  maxWidth?: string
+}
+
+type RowProps = {
+  marginTop?: string
+}
+
+export const Row = styled.div<RowProps>`
   display: flex;
   column-gap: 24px;
+
+  margin-top: ${(props) => props.marginTop || '0'};
 `
 
-export const InputGroup = styled.div`
+export const InputGroup = styled.div<InputGroupProps>`
   flex: auto;
+
+  max-width: ${(props) => props.maxWidth || 'auto'};
 
   label {
     font-size: 14px;
