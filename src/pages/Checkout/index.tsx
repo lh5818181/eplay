41f47dce6721efa9a78f756919a1d7a9524ccc4e
Luis <lh5818181@gmail.com>
2025-07-05 16:10:25 -0300

    validando checkout

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,104 +1,224 @@
 import { useState } from 'react'
 import Button from '../../components/Button'
 import Card from '../../components/Card'
-import { InputGroup, Row } from './styles'
+import { InputGroup, Row, TabButton } from './styles'
+
+import boleto from '../../assets//images/boleto.png'
+import cartao from '../../assets/images/cartao.png'
+import { useFormik } from 'formik'
 
 const Checkout = () => {
-  const [payWithCard, setPayWithCard] = useState(true)
+  const [payWithCard, setPayWithCard] = useState(false)
+
+  const form = useFormik({
+    initialValues: {
+      fullName: '',
+      email: '',
+      cpf: '',
+      deliveryEmail: '',
+      confirmDeliveryEmail: '',
+      cardOwner: '',
+      cpfcardOwner: '',
+      cardDisplayName: '',
+      cardNumber: '',
+      expiresMonth: '',
+      expiresYear: '',
+      cardCode: '',
+      installments: 1
+    },
+    onSubmit: (values) => {
+      console.log(values)
+    }
+  })
 
   return (
-    <div className="container">
+    <form onSubmit={form.handleSubmit} className="container">
       <Card title="Dados da cobrança">
         <>
           <Row>
             <InputGroup>
               <label htmlFor="fullName">Nome completo</label>
-              <input id="fullName" type="text" />
+              <input
+                id="fullName"
+                type="text"
+                name="fullName"
+                value={form.values.fullName}
+                onChange={form.handleChange}
+              />
             </InputGroup>
             <InputGroup>
               <label htmlFor="email">E-mail</label>
-              <input id="email" type="email" />
+              <input
+                id="email"
+                type="email"
+                name="email"
+                value={form.values.email}
+                onChange={form.handleChange}
+              />
             </InputGroup>
             <InputGroup>
               <label htmlFor="cpf">CPF</label>
-              <input id="cpf" type="text" />
+              <input
+                id="cpf"
+                type="text"
+                name="cpf"
+                value={form.values.cpf}
+                onChange={form.handleChange}
+              />
             </InputGroup>
           </Row>
           <h3 className="margin-top">Dados de entrega - conteúdo digital</h3>
           <Row>
             <InputGroup>
               <label htmlFor="deliveryEmail">E-mail</label>
-              <input type="email" id="deliveryEmail" />
+              <input
+                type="email"
+                id="deliveryEmail"
+                name="deliveryEmail"
+                value={form.values.deliveryEmail}
+                onChange={form.handleChange}
+              />
             </InputGroup>
             <InputGroup>
               <label htmlFor="confirmDeliveryEmail">Confirme o e-mail</label>
-              <input type="email" id="confirmDeliveryEmail" />
+              <input
+                type="email"
+                id="confirmDeliveryEmail"
+                name="confirmDeliveryEmail"
+                value={form.values.confirmDeliveryEmail}
+                onChange={form.handleChange}
+              />
             </InputGroup>
           </Row>
         </>
       </Card>
       <Card title="Pagamento">
-        <div>
-          {payWithCard ? (
-            <>
-              <Row>
-                <InputGroup>
-                  <label htmlFor="cardOwner">Nome do titular do cartão</label>
-                  <input type="text" id="cardOwner" />
-                </InputGroup>
-                <InputGroup>
-                  <label htmlFor="cpfcardOwner">CPF do titular do cartão</label>
-                  <input type="text" id="cpfcardOwner" />
-                </InputGroup>
-              </Row>
-              <Row marginTop="24px">
-                <InputGroup>
-                  <label htmlFor="cardDisplayName">Nome no cartão</label>
-                  <input type="text" id="cardDisplayName" />
-                </InputGroup>
-                <InputGroup>
-                  <label htmlFor="cardNumber">Número do cartão</label>
-                  <input type="text" id="cardNumber" />
-                </InputGroup>
-                <InputGroup maxWidth="123px">
-                  <label htmlFor="expiresMonth">Mês do vencimento</label>
-                  <input type="text" id="expiresMonth" />
-                </InputGroup>
-                <InputGroup maxWidth="123px">
-                  <label htmlFor="expiresYear">Ano de vencimento</label>
-                  <input type="text" id="expiresYear" />
-                </InputGroup>
-                <InputGroup maxWidth="48px">
-                  <label htmlFor="cardCode">CVV</label>
-                  <input type="text" id="cardCode" />
-                </InputGroup>
-              </Row>
-            </>
-          ) : (
-            <p>
-              Ao optar por essa forma de pagamento, é importante lembrar que a
-              confirmação pode levar até 3 dias úteis, devido aos prazos
-              estabelecidos pelas instituições financeiras. Portanto, a
-              liberação do código de ativação do jogo adquirido ocorrerá somente
-              após a aprovação do pagamento do boleto.
-            </p>
-          )}
-          <Row marginTop="24px">
-            <InputGroup maxWidth="116px">
-              <label htmlFor="installments">Parcelamento</label>
-              <select>
-                <option>1x de R$ 200,00</option>
-                <option>2x de R$ 200,00</option>
-                <option>3x de R$ 200,00</option>
-              </select>
-            </InputGroup>
-          </Row>
-        </div>
+        <>
+          <TabButton
+            isActive={!payWithCard}
+            onClick={() => setPayWithCard(false)}
+          >
+            <img src={boleto} alt="Boleto" />
+            Boleto bancário
+          </TabButton>
+          <TabButton
+            isActive={payWithCard}
+            onClick={() => setPayWithCard(true)}
+          >
+            <img src={cartao} alt="Cartão de crédito" />
+            Cartão de crédito
+          </TabButton>
+          <div className="margin-top">
+            {payWithCard ? (
+              <>
+                <Row>
+                  <InputGroup>
+                    <label htmlFor="cardOwner">Nome do titular do cartão</label>
+                    <input
+                      type="text"
+                      id="cardOwner"
+                      name="cardOwner"
+                      value={form.values.cardOwner}
+                      onChange={form.handleChange}
+                    />
+                  </InputGroup>
+                  <InputGroup>
+                    <label htmlFor="cpfcardOwner">
+                      CPF do titular do cartão
+                    </label>
+                    <input
+                      type="text"
+                      id="cpfcardOwner"
+                      name="cpfcardOwner"
+                      value={form.values.cpfcardOwner}
+                      onChange={form.handleChange}
+                    />
+                  </InputGroup>
+                </Row>
+                <Row marginTop="24px">
+                  <InputGroup>
+                    <label htmlFor="cardDisplayName">Nome no cartão</label>
+                    <input
+                      type="text"
+                      id="cardDisplayName"
+                      name="cardDisplayName"
+                      value={form.values.cardDisplayName}
+                      onChange={form.handleChange}
+                    />
+                  </InputGroup>
+                  <InputGroup>
+                    <label htmlFor="cardNumber">Número do cartão</label>
+                    <input
+                      type="text"
+                      id="cardNumber"
+                      name="cardNumber"
+                      value={form.values.cardNumber}
+                      onChange={form.handleChange}
+                    />
+                  </InputGroup>
+                  <InputGroup maxWidth="123px">
+                    <label htmlFor="expiresMonth">Mês do vencimento</label>
+                    <input
+                      type="text"
+                      id="expiresMonth"
+                      name="expiresMonth"
+                      value={form.values.expiresMonth}
+                      onChange={form.handleChange}
+                    />
+                  </InputGroup>
+                  <InputGroup maxWidth="123px">
+                    <label htmlFor="expiresYear">Ano de vencimento</label>
+                    <input
+                      type="text"
+                      id="expiresYear"
+                      name="expiresYear"
+                      value={form.values.expiresYear}
+                      onChange={form.handleChange}
+                    />
+                  </InputGroup>
+                  <InputGroup maxWidth="48px">
+                    <label htmlFor="cardCode">CVV</label>
+                    <input
+                      type="text"
+                      id="cardCode"
+                      name="cardCode"
+                      value={form.values.cardCode}
+                      onChange={form.handleChange}
+                    />
+                  </InputGroup>
+                </Row>
+              </>
+            ) : (
+              <p>
+                Ao optar por essa forma de pagamento, é importante lembrar que a
+                confirmação pode levar até 3 dias úteis, devido aos prazos
+                estabelecidos pelas instituições financeiras. Portanto, a
+                liberação do código de ativação do jogo adquirido ocorrerá
+                somente após a aprovação do pagamento do boleto.
+              </p>
+            )}
+            <Row marginTop="24px">
+              <InputGroup maxWidth="150px">
+                <label htmlFor="installments">Parcelamento</label>
+                <select
+                  id="installments"
+                  name="installments"
+                  value={form.values.installments}
+                  onChange={form.handleChange}
+                >
+                  <option>1x de R$ 200,00</option>
+                  <option>2x de R$ 200,00</option>
+                  <option>3x de R$ 200,00</option>
+                </select>
+              </InputGroup>
+            </Row>
+          </div>
+        </>
       </Card>
       <Button type="button" tittle="Clique aqui para finalizar a compra">
         Finalizar compra
       </Button>
-    </div>
+    </form>
   )
 }
 
